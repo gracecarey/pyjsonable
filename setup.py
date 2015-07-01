e0b072c740f3216ed03e3e1b275acc0c92198435
gcarey <gcarey@vaynermedia.com>
2015-07-01 18:02:51 -0400

    Modify setup file

--- setup.py
+++ setup.py
@@ -1,8 +1,15 @@
 from setuptools import setup, find_packages
 
+# -*- coding: utf-8 -*-
+from distutils.core import setup
+
 setup(
-    name = "pyjsonable",
-    version = "0.1",
-    packages = find_packages(),
-    url = "https://github.com/gracecarey/pyjsonable",
+    name='pyjsonable',
+    version='0.0.1',
+    author=u'Grace Carey',
+    author_email='gracecareymail@gmail.com',
+    packages=find_packages(),
+    url='https://github.com/gracecarey/pyjsonable',
+    license='BSD',
+    description='Native, validatable python objects serializable by json.dumps()',
 )
