98bd8038555f2e6f4b3b64a337e1217729d9a130
gcarey <gcarey@vaynermedia.com>
2015-07-02 12:04:51 -0400

    Reconfigure setup and egg files

--- setup.py
+++ setup.py
@@ -5,11 +5,19 @@ from distutils.core import setup
 
 setup(
     name='pyjsonable',
-    version='0.3',
+    version='0.4',
     author=u'Grace Carey',
     author_email='gracecareymail@gmail.com',
     packages=find_packages(),
     url='https://github.com/gracecarey/pyjsonable',
     license='BSD',
     description='Native, validatable python objects serializable by json.dumps()',
+    install_requires=[
+        'django>=1.7',
+        'openpyxl==2.2.1',
+        'python-dateutil',
+        'django-report-utils==0.3.4',
+        'djangorestframework>=3.0.4',
+    ],
+    zip_safe=False
 )
