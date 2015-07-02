18f3719c0cd0272ce188c2d5ae81c04e36dd27cc
gcarey <gcarey@vaynermedia.com>
2015-07-02 11:38:20 -0400

    Create egg to facilitate pip imports

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='pyjsonable',
-    version='0.0.2',
+    version='0.2',
     author=u'Grace Carey',
     author_email='gracecareymail@gmail.com',
     packages=find_packages(),
