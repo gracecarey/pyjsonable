8196b777d5b3ab1f77e5c269a3e5a010678a0120
gcarey <gcarey@vaynermedia.com>
2015-07-02 12:06:29 -0400

    Remove incorrect dependency files

--- setup.py
+++ setup.py
@@ -12,12 +12,5 @@ setup(
     url='https://github.com/gracecarey/pyjsonable',
     license='BSD',
     description='Native, validatable python objects serializable by json.dumps()',
-    install_requires=[
-        'django>=1.7',
-        'openpyxl==2.2.1',
-        'python-dateutil',
-        'django-report-utils==0.3.4',
-        'djangorestframework>=3.0.4',
-    ],
     zip_safe=False
 )
